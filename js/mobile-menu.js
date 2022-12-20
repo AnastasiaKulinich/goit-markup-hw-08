bca48beacaf2d353e7949c75ad9f674119eee29b
Anastasia Kulinich <anastasiakulinich@MacBook-Air-Anastasia.local>
2022-12-20 16:57:43 +0100

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,10 +1,10 @@
 (() => {
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-munu-close]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
-    menuList: document.querySelector (".menu"),
+    menuList: document.querySelector("mob_menu"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -15,6 +15,7 @@
     refs.menu.classList.toggle("is-hidden");
     refs.body.classList.toggle("no-scroll");
   }
+
   function removeMenu() {
     refs.menu.classList.add("is-hidden");
     refs.body.classList.remove("no-scroll");
